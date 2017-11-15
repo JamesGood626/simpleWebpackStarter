5b0b38a40f5d5b2755f653122749b11c45f11c14
James <jamesgood626@gmail.com>
2017-11-14 19:50:17 -0700

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path')
 
+
 const config = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js'
+  },
+  devServer: {
+    contentBase: './build'
+  },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
